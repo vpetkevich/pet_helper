53b42d24070b5a7bbdc1acaa837ac7bb4cda7dca
vpetkevich <62066192+vpetkevich@users.noreply.github.com>
2022-07-03 13:55:41 +0300

    Revert "added same menus for adding and editing and finished with handling bo…"

--- menus.py
+++ menus.py
@@ -12,20 +12,6 @@ cancel_menu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_cancel)
 search_menu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_optional_param, btn_cancel)
 
 
-pet_menu = {
-    'type': ReplyKeyboardMarkup(resize_keyboard=True).add('Кот', 'Собака', 'Другое', btn_cancel),
-    'gender': ReplyKeyboardMarkup(resize_keyboard=True).add('Мальчик', 'Девочка', btn_cancel),
-    'color': ReplyKeyboardMarkup(resize_keyboard=True).add('Белый', 'Черный', 'Рыжий', 'Трехцветный', 'Серый',
-                                                               'Лесной', 'Смешанный', btn_cancel),
-    'boolean': ReplyKeyboardMarkup(resize_keyboard=True).add('Да', 'Нет', btn_cancel),
-    'breed': ReplyKeyboardMarkup(resize_keyboard=True).add('Без породы', btn_cancel),
-    'district': ReplyKeyboardMarkup(resize_keyboard=True).add('Минская', 'Гомельская', 'Гродненская', 'Брестская',
-                                                                  'Могилевская'),
-    'description': ReplyKeyboardMarkup(resize_keyboard=True).add('Нет', btn_cancel),
-    'photos': ReplyKeyboardMarkup(resize_keyboard=True).add('Все фотографии добавлены', btn_cancel)
-}
-
-
 class FieldsMenu:
     fields_menu = ReplyKeyboardMarkup(resize_keyboard=True).add(
         db_fields['name'][1], db_fields['pet_type'][1], db_fields['age'][1], db_fields['gender'][1],
